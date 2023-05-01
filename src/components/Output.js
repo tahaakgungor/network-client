175034ba2ff7a5dcc1287702a8a9d874b863b72d
Taha Turan Akgüngör <tahaakgungor@gmail.com>
2023-05-01 17:24:17 +0300

    a

--- src/components/Output.js
+++ src/components/Output.js
@@ -2,33 +2,13 @@ import React, { useState, useEffect } from "react";
 import "../styles/Output.css";
 import { Button } from "antd";
 
-function Output({ output, socket, devices, children, activeTab }) {
+function Output({ output, children }) {
   const [commandOutput, setCommandOutput] = useState([]);
   const ref = React.useRef(null);
 
   React.useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth", block: "end" });
   }, [output, commandOutput]);
-
-  const handleCommandSubmit = (e, deviceId) => {
-    e.preventDefault();
-    try {
-      socket.emit("privateCommand", {
-        command: commandOutput[deviceId] || "",
-        deviceId,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-    setCommandOutput((prevOutput) => ({ ...prevOutput, [deviceId]: "" }));
-  };
-
-  const handleCommandChange = (e, deviceId) => {
-    setCommandOutput((prevOutput) => ({
-      ...prevOutput,
-      [deviceId]: e.target.value,
-    }));
-  };
 
   return (
     <div>
