23c62af8d1c5b6b2f6b407809054e78a0f37095b
Taha Turan Akgüngör <tahaakgungor@gmail.com>
2023-05-01 14:58:15 +0300

    inputproblem

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,63 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Output.css";
 import { Button } from "antd";
 
-
-function Output({ output, socket, devices }) {
-  const [command, setCommand] = useState("");
+function Output({ output, socket, devices, children, activeTab}) {
   const [commandOutput, setCommandOutput] = useState([]);
-
   const ref = React.useRef(null);
 
   React.useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth", block: "end" });
   }, [output, commandOutput]);
 
-
-  const handleCommandSubmit = (e) => {
+  const handleCommandSubmit = (e, deviceId) => {
     e.preventDefault();
     try {
-      devices.forEach((devices) => {
-        socket.emit("privateCommand", {
-          command,
-          devices,
-        });
+      socket.emit("privateCommand", {
+        command: commandOutput[deviceId] || "",
+        deviceId,
       });
     } catch (error) {
       console.error(error);
     }
-    setCommandOutput((prevOutput) => [...prevOutput, command]);
-    setCommand("");
+    setCommandOutput((prevOutput) => ({ ...prevOutput, [deviceId]: "" }));
   };
 
-  const handleCommandChange = (e) => {
-    setCommand(e.target.value);
+  const handleCommandChange = (e, deviceId) => {
+    setCommandOutput((prevOutput) => ({
+      ...prevOutput,
+      [deviceId]: e.target.value,
+    }));
   };
-
 
   return (
     <div>
       <div className="output-container">
         {output && <pre>{output}</pre>}
-        {commandOutput.map((cmdOutput, index) => (
-       <pre key={index}>{cmdOutput}</pre>
-        ))}
         <div ref={ref}></div>
       </div>
-      <form onSubmit={handleCommandSubmit}>
+      {children}
+      {activeTab && (
+      <form onSubmit={(e) => handleCommandSubmit(e, devices)}>
         <div className="output-input-button">
           <input
             className="form-control form-control-sm"
             type="text"
             placeholder="Enter command"
-            value={command}
-            onChange={handleCommandChange}
+            value={commandOutput[devices]}
+            onChange={(e) => handleCommandChange(e, devices)}
           />
           <Button type="primary" size="small" htmlType="submit">
             Send
           </Button>
         </div>
       </form>
+      )}
     </div>
   );
 }
--- src/pages/CommandPage.js
+++ src/pages/CommandPage.js
@@ -1,25 +1,24 @@
-import React, { useState, useLayoutEffect, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Output from "../components/Output";
 import { Tabs } from "antd";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { Button } from "antd";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/CommandPage.css";
 
 function CommandPage({ socket }) {
-  const [command, setCommand] = useState("");
+  const [commandStates, setCommandStates] = useState({});
   const [output, setOutput] = useState([]);
   const [deviceNames, setDeviceNames] = useState([]);
+  const [activeTab, setActiveTab] = useState(null);
 
   const location = useLocation();
 
-
-      const storedDevices = localStorage.getItem("cihazlar");
-      const devices = storedDevices ? JSON.parse(storedDevices) : location.state.cihazlar;
-
-
+  const storedDevices = localStorage.getItem("cihazlar");
+  const devices = storedDevices ? JSON.parse(storedDevices) : location.state.cihazlar;
 
   useEffect(() => {
     localStorage.setItem("lastVisitedPage", window.location.pathname);
@@ -29,7 +28,8 @@ function CommandPage({ socket }) {
     const initialOutput = devices.map((id) => ({ id, output: "" }));
     setOutput(initialOutput);
 
-
+    const initialCommandStates = devices.reduce((acc, deviceId) => ({ ...acc, [deviceId]: "" }), {});
+    setCommandStates(initialCommandStates);
 
     devices.forEach((deviceId) => {
       console.log("output" + deviceId);
@@ -58,58 +58,71 @@ function CommandPage({ socket }) {
         devices.map(async (id) => {
           const response = await axios.get(
             `${process.env.REACT_APP_BACKEND_URL}devices/selected/${id}`
-          );
-          return response.data[0].name;
-        })
-      );
-      setDeviceNames(names);
-    };
-
-    fetchDeviceNames();
-  }, [devices]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      devices.forEach((deviceId) => {
-        socket.emit("command", {
-          command,
-          deviceId,
-        });
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleChange = (e) => {
-    setCommand(e.target.value);
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Command:
-          <input type="text" value={command} onChange={handleChange} />
-        </label>
-        <button className="btn-success" type="submit">
-          Submit
-        </button>
-      </form>
-      <Tabs
-        className="tabs"
-        items={output.map(({ id, output, input}, index) => ({
-          key: index,
-          label: deviceNames[index] || id,
-          children: <Output output={output} socket={socket} devices={devices} input={input}/>,
-          
-        }))}
-        
-      />
-
-    </div>
-  );
-}
-
-export default CommandPage;
+            );
+            return response.data[0].name;
+            })
+            );
+            setDeviceNames(names);
+            };
+            
+       
+            fetchDeviceNames();
+            }, [devices]);
+            
+            const handleSubmit = async (e, deviceId) => {
+            e.preventDefault();
+            try {
+            socket.emit("command", {
+            command: commandStates[deviceId],
+            deviceId,
+            });
+            } catch (error) {
+            console.error(error);
+            }
+            };
+            
+            const handleChange = (e, deviceId) => {
+            setCommandStates((prevState) => ({
+            ...prevState,
+            [deviceId]: e.target.value,
+            }));
+            };
+            
+            return (
+            <div>
+            <Tabs
+            className="tabs"
+            items={output.map(({ id, output, input }, index) => ({
+            key: index,
+            label: deviceNames[index] || id,
+            children: (
+            <Output
+                       output={output}
+                       socket={socket}
+                       devices={devices}
+                       input={input}
+                       activeTab={activeTab}
+                     >
+            <form onSubmit={(e) => handleSubmit(e, id)}>
+            <div className="output-input-button">
+            <input
+            className="form-control form-control-sm"
+            type="text"
+            placeholder="Enter command"
+            value={commandStates[id]}
+            onChange={(e) => handleChange(e, id)}
+            />
+            <Button type="primary" size="small" htmlType="submit">
+            Send
+            </Button>
+            </div>
+            </form>
+            </Output>
+            ),
+            }))}
+            />
+            </div>
+            );
+            }
+            
+            export default CommandPage;
